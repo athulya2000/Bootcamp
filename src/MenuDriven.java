1294667e674b73c473f292306e21e13ccdb5fb43
athulya2000 <athulyav00@gmail.com>
2022-11-21 10:35:46 +0530

    second commit

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class MenuDriven {
     public static void main(String[] args) {
+        System.out.println("Please select number");
+        System.out.println(("menu:1.Addition\n 2.Subtraction \n 3.Multiplication \n 4.Division"));
+        System.out.println(("Enter a two number"));
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        int numb=4;
+
+
 
     }
 }
